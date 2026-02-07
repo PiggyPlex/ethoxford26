d9a617941fb81262b6fde7708edcda4dbf756b2f
Aideon Quek <15018948+PiggyPlex@users.noreply.github.com>
2026-02-07 19:44:47 +0000

    feat: add fashion news

--- engine/src/agents/chat.ts
+++ engine/src/agents/chat.ts
@@ -1,4 +1,4 @@
-import { createAgent } from "langchain";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { getWeather } from "../tools/weather";
 import { webSearch, visitPage } from "../tools/webSearch";
 import { listFiles, readFile, writeFile } from "../tools/fileTools";
@@ -7,10 +7,11 @@ import { fetchProactiveActionsTool, saveProactiveActionTool } from "../tools/pro
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { MultiServerMCPClient } from "@langchain/mcp-adapters";  
 import { resolve } from "node:path";
-import { suggestContentTool } from "tools/suggest_content";
-import { createFact, readFacts } from "tools/facts";
-import { getWindows } from "tools/windows";
-import { fetchContextNotesTool } from "tools/context_notes";
+import { suggestContentTool } from "../tools/suggest_content";
+import { createFact, readFacts } from "../tools/facts";
+import { getWindows } from "../tools/windows";
+import { fetchContextNotesTool } from "../tools/context_notes";
+import { getFashionLifestyleNews } from "../tools/FashionLifestyleNews";
 
 const llm = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
@@ -18,7 +19,7 @@ const llm = new ChatGoogleGenerativeAI({
 
 const client = new MultiServerMCPClient({  
   googleCalendar: {
-    transport: "stdio",  // Local subprocess communication
+    transport: "stdio",
     command: "bunx",
     args: ["@cocal/google-calendar-mcp"],
     env: {
@@ -26,24 +27,24 @@ const client = new MultiServerMCPClient({
     },
   },
   spotify: {
-    "command": "uvx",
-    "args": [
+    command: "uvx",
+    args: [
       "--python", "3.12",
       "--from", "git+https://github.com/varunneal/spotify-mcp",
       "spotify-mcp"
     ],
-    "env": {
-      "SPOTIFY_CLIENT_ID": process.env.SPOTIFY_CLIENT_ID!,
-      "SPOTIFY_CLIENT_SECRET": process.env.SPOTIFY_CLIENT_SECRET!,
-      "SPOTIFY_REDIRECT_URI": "http://127.0.0.1:8080/callback"
+    env: {
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID!,
+      SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET!,
+      SPOTIFY_REDIRECT_URI: "http://127.0.0.1:8080/callback"
     }
   },
 });
 
 const mcpTools = await client.getTools();
 
-export const planningAgent = createAgent({
-  model: llm,
+export const planningAgent = createReactAgent({
+  llm,
   tools: [
     getWeather, 
     webSearch,
@@ -59,5 +60,7 @@ export const planningAgent = createAgent({
     getWindows,
     fetchContextNotesTool,
     fetchProactiveActionsTool,
+    saveProactiveActionTool,
+    getFashionLifestyleNews,
   ],
 });
